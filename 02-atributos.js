b57b60746210f93e06a04904bee141d76b3d553b
GabMoreira123 <gabrielsmoreira49@gamil.com>
2024-09-09 08:57:11 -0400

    add metodos.js

---
+++ 02-atributos.js
@@ -0,0 +1,9 @@
+class Pessoa {
+    constructor(nome, idade, atleta, peso) { // atributos 
+        // Inicialização dos atributos
+        this.nome =nome;
+        this.idade = idade;
+        this.altura = this.altura;
+        this.peso = peso;
+    }
+}
---
+++ 03-metodos.js
@@ -0,0 +1,13 @@
+class pessoa {
+    constructor(nome, altura, peso, genero){ // Atributos
+        this.nome =nome;
+        this.idade = idade;
+        this.altura = this.altura;
+        this.peso = peso;
+
+    }
+    //Métodos
+    falar() {
+        console.log('ola, meu nome é ${this.nome}!');
+    }
+}
