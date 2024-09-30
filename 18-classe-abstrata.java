96305e4bf75952ff2cc5a1e8726a4cdcee254c36
Geisiany <geisianycristiny7@gmail.com>
2024-09-30 09:54:29 -0400

    add classe Guerreiro

--- 18-classe-abstrata.java
+++ 18-classe-abstrata.java
@@ -11,3 +11,28 @@ abstrata class personagem {
     public abstract void atacar();
     public abstract void defender();
 }
+
+// classe derivada "Guerreiro"
+class Guerreiro extends personagem {
+    privata String arma;
+
+    // construtor da classe Guerreiro
+    public Guerreiro(String, String arma) {
+        super(nome);
+        this.arma = arma;
+    }
+
+    // sobrescrita do metodo atacar da classe abstrata personagem
+    @Override
+    public void atacar() {
+        system.out.println(nome + "ataca com a " + arma + "!");
+    }
+
+    // sobrescrita do metodo defender da classe abstrata personagem
+    @Override
+    public void defender() {
+        system.out.println(nome + "levanta o escudo para se defender!");
+
+    
+    }
+}
